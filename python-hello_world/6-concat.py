d81127e8146e128dfc49c539c1c6d8c5d9594640
HuseynSadatkhanovHolberton <12671@holbertonstudents.com>
2026-04-06 12:35:07 +0000

    task 4 6-concat

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Holberton School!"
-
-# Complete this line
-print(f"{str1}{str2}")
+str1 = "Holberton"
+str2 = "School"
+full_str = str1 + " " + str2
+print(f"Welcome to {full_str}!")
